e4b203871389970aac052f3283e714728774f68d
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2024-02-05 02:43:58 +0200

    Added Ability to select Project when creating Task

--- src/taskHandler.js
+++ src/taskHandler.js
@@ -4,34 +4,61 @@ import { createTaskUi } from "./ui.js";
 import { createAndPopulateDetailsModal } from "./ui.js";
 import { createProjectCreationEl } from "./ui.js";
 
+const {
+  taskModal,
+  titleH3,
+  titleInput,
+  descriptionH3,
+  descriptionInput,
+  dueDateH3,
+  dueDateInput,
+  priorityH3,
+  highPriorityInput,
+  highPriorityLabel,
+  mediumPriorityInput,
+  mediumPriorityLabel,
+  lowPriorityInput,
+  lowPriorityLabel,
+  projectH3,
+  projectInput,
+  doneBtn,
+} = createTaskCreationEl();
+
+function ProjectHandler() {
+  const nav = document.querySelector(".nav");
+  const projectsArray = [];
+  const addProjectBtn = document.querySelector(".new-project-btn");
+  const { projectInputContainer, projectNameInput, submitProjectBtn } =
+    createProjectCreationEl();
+  function handleProjectCreation() {
+    nav.appendChild(projectInputContainer);
+  }
+  addProjectBtn.addEventListener("click", handleProjectCreation);
+  submitProjectBtn.addEventListener("click", () => {
+    const projectName = projectNameInput.value;
+    projectsArray.push(projectName);
+    const projectH3 = document.createElement("h3");
+    projectH3.textContent = projectName;
+    nav.removeChild(projectInputContainer);
+    nav.appendChild(projectH3);
+    projectNameInput.value = "";
+    const newOption = document.createElement("option");
+    newOption.textContent = projectName;
+    projectInput.appendChild(newOption)
+
+  });
+}
+ProjectHandler();
+
 export default function createTask() {
   const addTaskBtn = document.querySelector(".add-task-btn");
   const mainTasksContainer = document.querySelector(".main-tasks-container");
-  const {
-    taskModal,
-    titleH3,
-    titleInput,
-    descriptionH3,
-    descriptionInput,
-    dueDateH3,
-    dueDateInput,
-    priorityH3,
-    highPriorityInput,
-    highPriorityLabel,
-    mediumPriorityInput,
-    mediumPriorityLabel,
-    lowPriorityInput,
-    lowPriorityLabel,
-    projectH3,
-    projectInput,
-    doneBtn,
-  } = createTaskCreationEl();
   function handleAddTask() {
     taskModal.appendChild(titleH3);
     taskModal.appendChild(titleInput);
     taskModal.appendChild(descriptionH3);
     taskModal.appendChild(descriptionInput);
-    taskModal.appendChild(dueDateH3)
+    taskModal.appendChild(dueDateH3);
     taskModal.appendChild(dueDateInput);
     taskModal.appendChild(priorityH3);
     taskModal.appendChild(highPriorityInput);
@@ -51,13 +78,15 @@ export default function createTask() {
     const title = titleInput.value;
     const description = descriptionInput.value;
     const dueDate = dueDateInput.value;
+    const project = projectInput.value;
     if (!title) {
       alert("Please enter a title for the task.");
       return;
     } else if (title) {
       if (document.querySelector('input[name="priority"]:checked') == null) {
         const priority = "None";
-        const task = new Task(title, description, dueDate, priority, false);
+        const task = new Task(title, description, dueDate, priority, project, false);
+        console.log(task)
         titleInput.value = "";
         descriptionInput.value = "";
         dueDateInput.value = "";
@@ -117,23 +146,3 @@ export default function createTask() {
   addTaskBtn.addEventListener("click", handleAddTask);
   doneBtn.addEventListener("click", handleDoneBtn);
 }
-function ProjectHandler() {
-  const nav = document.querySelector(".nav");
-  const projectsArray = [];
-  const addProjectBtn = document.querySelector(".new-project-btn");
-  const {projectInputContainer, projectNameInput, submitProjectBtn} = createProjectCreationEl();
-  function handleProjectCreation() {
-    nav.appendChild(projectInputContainer);
-  }
-  addProjectBtn.addEventListener("click",handleProjectCreation);
-  submitProjectBtn.addEventListener("click",() => {
-    const projectName = projectNameInput.value;
-    projectsArray.push(projectName)
-    const projectH3 = document.createElement("h3");
-    projectH3.textContent = projectName;
-    nav.removeChild(projectInputContainer);
-    nav.appendChild(projectH3);
-    projectNameInput.value = "";
-  });
-}
-ProjectHandler()
